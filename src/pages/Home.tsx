f17cd34511b4eed56c29a4ef88df6455da6972a7
Luna5akura <734514717@qq.com>
2024-12-10 19:46:56 +0800

    feat: sidebar update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,36 @@
 // src/pages/Home.tsx
-import React from 'react';
 import BlogList from '@/components/BlogList';
+import {useLocation} from "react-router-dom";
+import {posts} from "@/data/posts";
+import Sidebar from "@/components/Sidebar";
 
 const Home: React.FC = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const selectedCategory = params.get('category');
+  const allCategories = Array.from(new Set(posts.map((post) => post.category)));
+
+  const filteredPosts = selectedCategory && selectedCategory !== 'All'
+  ? posts.filter((post) => post.category === selectedCategory)
+  : posts;
+
+  const hasPosts = filteredPosts.length > 0;
+
   return (
     <div className="container mx-auto mt-10">
       <h1 className="text-6xl font-bold mb-8 text-center text-sky-100">This is Luna's world</h1>
-      <BlogList />
+      <div className="flex row-auto">
+        <div className="w-1/6 m-6 h-screen">
+          <Sidebar categories={allCategories}/>
+        </div>
+        <div className="w-5/6">
+          {hasPosts ? (
+            <BlogList posts={filteredPosts}/>
+          ) : (
+            <p className="text-sky-100">No posts found in this category.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
