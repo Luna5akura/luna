befc11fa29abf026dc95261a8a4617d3035fdfab
Luna5akura <734514717@qq.com>
2024-12-10 19:07:24 +0800

    feat: change base

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/luna/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
