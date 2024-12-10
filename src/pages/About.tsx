2f6a025d9248301d04e04d0622852a15e81d6cf3
Luna5akura <734514717@qq.com>
2024-12-10 20:17:54 +0800

    feat:sidebar update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <div className="mx-80 mt-10 p-6 bg-sky-100 rounded-lg shadow">
-      <h1 className="text-3xl font-bold mb-4 text-sky-950">About</h1>
-      <p className="text-sky-950 leading-7">
+      <h1 className="text-3xl font-bold mb-4 text-sky-900">About</h1>
+      <p className="text-sky-900 leading-7">
         Love is murderous utopia.
       </p>
     </div>
