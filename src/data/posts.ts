6ca2477013b1f4daf6c34570ed294ea19ded1b9e
Luna5akura <734514717@qq.com>
2024-12-11 03:43:47 +0800

    qc:day 2

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -31,11 +31,20 @@ export const posts: Post[] = [
   },
   {
     id:4,
-    title: '[QC] Day 1- Projective measure',
+    title: '[QC] Day 1 - Projective measure',
     date: '2024-12-10',
     excerpt: 'QC Day 1 - Projective measure: Exercise 2.58 - 2.61',
     contentKey: 'QC_1',
     author: 'Me',
     category: 'Quantum Computation',
-  }
+  },
+  {
+    id:5,
+    title: '[QC] Day 2 - Phase, Composite systems',
+    date: '2024-12-10',
+    excerpt: 'QC Day 2 - Phase Composite systems',
+    contentKey: 'QC_2',
+    author: 'Me',
+    category: 'Quantum Computation',
+  },
 ];
