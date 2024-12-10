57d50eef9abc1609bebe5884ca879e945bc4d45f
Luna5akura <734514717@qq.com>
2024-12-10 23:19:45 +0800

    qc:day 1

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -18,7 +18,7 @@ export const posts: Post[] = [
     excerpt: 'Test Post 2',
     contentKey: 'Test Post 2',
     author: 'Tester',
-    category: 'test'
+    category: 'test',
   },
   {
     id: 3,
@@ -27,6 +27,15 @@ export const posts: Post[] = [
     excerpt: 'Test Post 3',
     contentKey: 'Test Post 3',
     author: 'Tester',
-    category: 'test'
+    category: 'test',
   },
+  {
+    id:4,
+    title: '[QC] Day 1- Projective measure',
+    date: '2024-12-10',
+    excerpt: 'QC Day 1 - Projective measure: Exercise 2.58 - 2.61',
+    contentKey: 'QC_1',
+    author: 'Me',
+    category: 'Quantum Computation',
+  }
 ];
