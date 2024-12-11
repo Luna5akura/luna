d52a7fa20fbfa69618c373acc7163ab54660cd33
Luna5akura <734514717@qq.com>
2024-12-11 18:32:39 +0800

    cd:1

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,11 +24,17 @@ const Sidebar: React.FC<SidebarProps> = ({ categories }) => {
 
   return (
     <div className="fixed top-auto left-4 h-auto w-min p-4 bg-sky-100 rounded-lg shadow">
-      <h2 className="text-xl font-semibold text-sky-900 mb-4">Categories</h2>
+      <h2 className="font-semibold text-sky-900 mb-4
+      lg:text-xl
+      sm:text-base
+      ">Categories</h2>
       <ul>
         <li className="mb-2">
           <Button
             className={`w-full justify-start font-semibold hover:bg-sky-600 focus:outline-none
+            text-xs
+            sm:text-xs
+            lg:text-sm
             ${selectedCategory === null 
               ? 'bg-sky-900 text-sky-100' 
               : 'bg-sky-100 text-sky-900'}`}
@@ -40,7 +46,10 @@ const Sidebar: React.FC<SidebarProps> = ({ categories }) => {
         {categories.map((category) => (
           <li key={category} className="mb-2">
             <Button
-              className={`w-full justify-start font-semibold hover:bg-sky-600 focus:outline-none
+              className={`w-full justify-start font-semibold hover:bg-sky-600 focus:outline-none text-left
+              text-xs break-words whitespace-normal
+              sm:text-xs sm:break-words sm:whitespace-normal 
+              lg:text-sm lg:break-normal lg:whitespace-nowrap 
               ${selectedCategory === category 
                 ? 'bg-sky-900 text-sky-100' 
                 : 'bg-sky-100 text-sky-900'}`}
