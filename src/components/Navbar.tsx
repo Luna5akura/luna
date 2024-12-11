6e1a52720b12c5405bc9758c332a29c8ebb4a002
Luna5akura <734514717@qq.com>
2024-12-12 02:53:15 +0800

    feat: world update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,38 @@
 // src/components/Navbar.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Switch } from "@/components/ui/switch";
+import { useFontToggle } from "@/context/FontToggleContext";
 
 const Navbar: React.FC = () => {
+  const { fontToggle, setFontToggle } = useFontToggle();
+
+  React.useEffect(() => {
+    const root = window.document.documentElement;
+    if (fontToggle) {
+      root.classList.add('alt-font');
+    } else {
+      root.classList.remove('alt-font');
+    }
+  }, [fontToggle]);
+
   return (
     <nav className="bg-sky-100 shadow">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link to="/" className="text-xl font-bold text-sky-900 hover:text-sky-600">
           Luna
         </Link>
-        <div>
+        <div className="my-auto flex items-center justify-between">
+          <Switch
+            className="mr-4"
+            checked={fontToggle}
+            onCheckedChange={setFontToggle}
+          />
           <Link to="/warp" className="font-semibold text-sky-900 hover:text-sky-600 mx-4">
             Warp
           </Link>
-          <Link to="/" className="font-semibold text-sky-900 hover:text-sky-600 mx-4">
-            Home
+          <Link to="/show" className="font-semibold text-sky-900 hover:text-sky-600 mx-4">
+            World
           </Link>
           <Link to="/about" className="font-semibold text-sky-900 hover:text-sky-600 mx-4">
             About
