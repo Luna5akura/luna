d889fdf7317e31eeee88f40d6c355e5aefa614d6
luna5akura <734514717@qq.com>
2024-12-27 22:54:34 +0800

    feat: hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
 // src/App.tsx
-import { Routes, Route } from 'react-router-dom';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import Home from '@/pages/Home';
-import About from '@/pages/About';
-import PostDetails from '@/pages/PostDetails';
+import { Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import PostDetails from "@/pages/PostDetails";
 import Warp from "@/pages/Warp";
 import Show from "@/pages/Show";
-import {FontToggleProvider} from "@/context/FontToggleContext";
+import { FontToggleProvider } from "@/context/FontToggleContext";
 
 function App() {
   return (
-    <FontToggleProvider>
-      <div className="flex flex-col w-screen min-h-screen bg-sky-600">
-        <Navbar/>
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/warp" element={<Warp/>}/>
-            <Route path="/show" element={<Show/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/posts/:id" element={<PostDetails/>}/>
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </FontToggleProvider>
+    <HashRouter>
+      <FontToggleProvider>
+        <div className="flex flex-col w-screen min-h-screen bg-sky-600">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/warp" element={<Warp />} />
+              <Route path="/show" element={<Show />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/posts/:id" element={<PostDetails />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </FontToggleProvider>
+    </HashRouter>
   );
 }
 
