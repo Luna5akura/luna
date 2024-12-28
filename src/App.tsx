7f48595e21a339ae30acb2237f10fd48f97255f3
luna5akura <734514717@qq.com>
2024-12-28 18:44:00 +0800

    test: hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Home from "@/pages/Home";
@@ -12,19 +12,21 @@ import { FontToggleProvider } from "@/context/FontToggleContext";
 function App() {
   return (
       <FontToggleProvider>
-        <div className="flex flex-col w-screen min-h-screen bg-sky-600">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/warp" element={<Warp />} />
-              <Route path="/show" element={<Show />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/posts/:id" element={<PostDetails />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <HashRouter>
+          <div className="flex flex-col w-screen min-h-screen bg-sky-600">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/warp" element={<Warp />} />
+                <Route path="/show" element={<Show />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/posts/:id" element={<PostDetails />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </HashRouter>
       </FontToggleProvider>
   );
 }
