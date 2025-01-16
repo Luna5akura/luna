bd0855ff8a0d10869a52ff53897d0aca219a9d2c
luna5akura <734514717@qq.com>
2025-01-16 22:23:32 +0800

    update: ec3

--- src/pages/Warp.tsx
+++ src/pages/Warp.tsx
@@ -64,6 +64,19 @@ const Warp: React.FC = () => {
         {/*  Love is murderous utopia.*/}
         {/*</p>*/}
       </div>
+      <div className="mx-auto w-1/2 mt-10 p-6 bg-sky-100 rounded-lg shadow">
+        <a
+          href="https://www.sapium.site/wordpress/"
+          className="font-bold mb-0.5 text-sky-900
+            text-xl
+            sm:text-xl
+            lg:text-3xl
+            "
+        >乌桕Sapium</a>
+        {/*<p className="text-sky-900 leading-7">*/}
+        {/*  Love is murderous utopia.*/}
+        {/*</p>*/}
+      </div>
     </div>
   );
 };
